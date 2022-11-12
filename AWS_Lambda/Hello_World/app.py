517fa90ab2f332d3ca353253d4360095164f541c
Ahmed Tariq <78534449+ahmedtariq01@users.noreply.github.com>
2022-11-12 16:47:16 +0500

    Update app.py

--- AWS_Lambda/Hello_World/app.py
+++ AWS_Lambda/Hello_World/app.py
@@ -7,6 +7,6 @@ from hello_world.hello_world_stack import HelloWorldStack
 
 app = cdk.App()
 HelloWorldStack(app, "hello-world",
-    env=cdk.Environment(account='315997497220', region='us-east-2'),)
+    env=cdk.Environment(account='123456789', region='us-east-2'),)
 
 app.synth()
