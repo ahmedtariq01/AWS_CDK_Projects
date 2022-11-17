5316ce8f7d20a7b6473a4a98050eaaa6a5a64e64
Ahmed Tariq <ahmed.tm097@gmail.com>
2022-11-17 11:54:21 +0500

    Bug fixed

---
+++ AWS_Lambda/Simple_Web_HealthCheck/README.md
@@ -0,0 +1,83 @@
+# AWS Lambda Webapp Healthcheck CDK Pyhton Project
+
+This project demonstrates a CDK app with an instance of a stack (`web_health_check_stack`). This projects creates a Lambda Function that checks the `availability` and `latency` of the websites. Also, Convert the lambda function into a cron job on 60 min intervals. The `boto3` will be used to create the CloudWatch metrics and alarm.
+
+## Architecture Diagram:
+
+<div align="center">
+   <div align="center">
+    <img src="Architecture_Diagram/AWS_Lambda_HealthCheck_CornJob.jpg" width='700'/>
+  </div>
+</div>
+</br>
+
+
+The `cdk.json` file tells the CDK Toolkit how to execute your app.
+
+This project is set up like a standard Python project.  The initialization process also creates
+a virtualenv within this project, stored under the .venv directory.  To create the virtualenv
+it assumes that there is a `python3` executable in your path with access to the `venv` package.
+If for any reason the automatic creation of the virtualenv fails, you can create the virtualenv
+manually once the init process completes.
+
+To manually create a virtualenv on MacOS and Linux:
+
+```
+$ python -m venv .venv
+```
+
+After the init process completes and the virtualenv is created, you can use the following
+step to activate your virtualenv.
+
+```
+$ source .venv/bin/activate
+```
+
+If you are a Windows platform, you would activate the virtualenv like this:
+
+```
+% .venv\Scripts\activate.bat
+```
+
+Once the virtualenv is activated, you can install the required dependencies.
+
+```
+$ pip install -r requirements.txt
+```
+
+At this point you can now synthesize the CloudFormation template for this code.
+
+```
+$ cdk synth
+```
+
+You can now begin exploring the source code, contained in the hello directory.
+There is also a very trivial test included that can be run like this:
+
+```
+$ pytest
+```
+
+To add additional dependencies, for example other CDK libraries, just add to
+your requirements.txt file and rerun the `pip install -r requirements.txt`
+command.
+
+## Useful commands
+
+ * `cdk ls`          list all stacks in the app
+ * `cdk synth`       emits the synthesized CloudFormation template
+ * `cdk deploy`      deploy this stack to your default AWS account/region
+ * `cdk diff`        compare deployed stack with current state
+ * `cdk docs`        open CDK 
+ 
+ ## Useful links
+
+ - AWS Lambda: https://docs.aws.amazon.com/cdk/api/v1/python/aws_cdk.aws_lambda/README.html
+ - AWS Lamda IAM Role: https://docs.aws.amazon.com/cdk/api/v1/python/aws_cdk.aws_iam/Role.html
+ - AWS Events: https://docs.aws.amazon.com/cdk/api/v1/python/aws_cdk.aws_events/README.html
+ - AWS Events Targets: https://docs.aws.amazon.com/cdk/api/v1/python/aws_cdk.aws_events_targets/README.html
+ - AWS CloudWatch: https://docs.aws.amazon.com/cdk/api/v1/python/aws_cdk.aws_cloudwatch/README.html
+ - AWS CloudWatch Alarm: https://docs.aws.amazon.com/cdk/api/v1/python/aws_cdk.aws_cloudwatch/Alarm.html
+ - Boto3: https://boto3.amazonaws.com/v1/documentation/api/latest/index.html
+ - Boto3 CloudWatch: https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/cloudwatch.html
+ 
---
+++ AWS_Lambda/Simple_Web_HealthCheck/web_health_check/web_health_check_stack.py
@@ -0,0 +1,100 @@
+from aws_cdk import (
+    Duration,
+    aws_lambda as lambda_ ,
+    aws_events as events_,
+    aws_events_targets as targets_,
+    Stack,
+    RemovalPolicy,
+    aws_cloudwatch as cloudwatch_,
+    aws_iam as iam_,
+)
+from constructs import Construct
+from src import constants as const
+
+class WebHealthCheckStack(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+        
+        # lambda role
+        lambda_role = self.create_lambda_role()
+        
+        # The code that defines your stack goes here
+        fn = self.create_lambda('WebHealthCheck','./src','web_health_check.lambda_handler',lambda_role)
+        
+        # destroy the lambda function when the stack is destroyed
+        fn.apply_removal_policy(RemovalPolicy.DESTROY)
+         
+        # create a rule to trigger the lambda function every 60 minutes
+        schedule=events_.Schedule.rate(Duration.minutes(60))
+        
+        # defines the target function
+        targets=targets_.LambdaFunction(handler=fn)
+        
+        # corn job rule to trigger the lambda function
+        rule = events_.Rule(self, "WebHealthRule",
+            schedule = schedule,
+            targets = [targets]
+        )
+ 
+        # destroy the rule when the stack is destroyed
+        rule.apply_removal_policy(RemovalPolicy.DESTROY)
+        
+        # creating the cloud Watch alarm for the availability metric
+        dimensions = {'URls': str(url) for url in const.urls}
+        
+        avaiilability_metric = cloudwatch_.Metric(
+            metric_name=const.availability_metric,
+            namespace = const.namespace,
+            dimensions_map= dimensions,
+        )
+        avaiilability_alarm =  cloudwatch_.Alarm(self, "Availability_Error",
+            metric=avaiilability_metric,
+            evaluation_periods=60,
+            threshold=1,
+            comparison_operator=cloudwatch_.ComparisonOperator.LESS_THAN_THRESHOLD, 
+        
+        )
+        
+        # creating the cloud Watch alarm for the latency metric
+        dimensions = {'URls': str(url) for url in const.urls}
+        
+        latency_metric = cloudwatch_.Metric(
+            metric_name=const.latency_metric,
+            namespace = const.namespace,
+            dimensions_map= dimensions,
+        )
+        latency_alarm =  cloudwatch_.Alarm(self, "Latency_Errors",
+            metric=latency_metric,
+            evaluation_periods=60,
+            threshold=0.5,
+            comparison_operator=cloudwatch_.ComparisonOperator.GREATER_THAN_THRESHOLD, 
+        
+        )
+
+    # creating a lambda function
+    def create_lambda(self, id, asset, handler, role):
+        return lambda_.Function(self,
+        id = id,
+        handler = handler,
+        code = lambda_.Code.from_asset(asset),
+        runtime=lambda_.Runtime.PYTHON_3_9)
+
+    # creating a lambda role
+    def create_lambda_role(self):
+        lambdaRole = iam_.Role(self, "Lambda_Role",
+            assumed_by=iam_.ServicePrincipal("lambda.amazonaws.com"),
+            managed_policies=[
+              iam_.ManagedPolicy.from_aws_managed_policy_name("service-role/AWSLambdaBasicExecutionRole"),  
+              iam_.ManagedPolicy.from_aws_managed_policy_name("CloudWatchFullAccess"),
+                
+            ])
+        return lambdaRole
+    
+    
+    
+    
+    
+    
+    
+    
