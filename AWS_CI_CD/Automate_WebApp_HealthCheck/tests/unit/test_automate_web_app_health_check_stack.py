a388878a5f7a746bf1734f7393d17939a9c91ff4
Ahmed Tariq <ahmed.tm097@gmail.com>
2022-11-25 21:37:12 +0500

    Adding pytest fixture

--- AWS_CI_CD/Automate_WebApp_HealthCheck/tests/unit/test_automate_web_app_health_check_stack.py
+++ AWS_CI_CD/Automate_WebApp_HealthCheck/tests/unit/test_automate_web_app_health_check_stack.py
@@ -5,25 +5,31 @@ import pytest
 from automate_web_app_health_check.automate_web_app_health_check_stack import AutomateWebAppHealthCheckStack
 
  
+@pytest.fixture
+def app():
+    app = core.App()
+    return app
+
+
 # code ref: https://docs.aws.amazon.com/cdk/api/v1/python/aws_cdk.assertions/Template.html
 # check wheteher lambda is created or not
-def test_lambda():
-    app = core.App()
+def test_lambda(app):
+    app = app
     stack = AutomateWebAppHealthCheckStack(app, "automate-web-app-health-check")
     template = assertions.Template.from_stack(stack)
     template.resource_count_is("AWS::Lambda::Function", 2)
    
     
 # Check whether SNS is created or not
-def test_sns():
-    app = core.App()
+def test_sns(app):
+    app = app
     stack = AutomateWebAppHealthCheckStack(app, "automate-web-app-health-check")
     template = assertions.Template.from_stack(stack)
     template.resource_count_is("AWS::SNS::Topic", 1)
     
 # check the lambda handler function name
-def test_lambda_handler():
-    app = core.App()
+def test_lambda_handler(app):
+    app = app
     stack = AutomateWebAppHealthCheckStack(app, "automate-web-app-health-check")
     template = assertions.Template.from_stack(stack)
     template.has_resource_properties("AWS::Lambda::Function", {
@@ -32,22 +38,22 @@ def test_lambda_handler():
     })
 
 # Check whether DB table is created or not 
-def test_dynamodb():
-    app = core.App()
+def test_dynamodb(app):
+    app = app
     stack = AutomateWebAppHealthCheckStack(app, "automate-web-app-health-check")
     template = assertions.Template.from_stack(stack)
     template.resource_count_is("AWS::DynamoDB::Table", 1)
     
 # check the number of alarms created in CloudWatch
-def test_cloudwatch():
-    app = core.App()
+def test_cloudwatch(app):
+    app = app
     stack = AutomateWebAppHealthCheckStack(app, "automate-web-app-health-check")
     template = assertions.Template.from_stack(stack)
     template.resource_count_is("AWS::CloudWatch::Alarm", 2)
 
 # check the SNS subscriptions email
-def test_SNS_Subscription():
-    app = core.App()
+def test_SNS_Subscription(app):
+    app = app
     stack = AutomateWebAppHealthCheckStack(app, "automate-web-app-health-check")
     template = assertions.Template.from_stack(stack)
     template.has_resource_properties("AWS::SNS::Subscription", {
@@ -55,29 +61,29 @@ def test_SNS_Subscription():
     })
     
 # check the number of rules created in CloudWatch
-def test_cloudwatch_rule():
-    app = core.App()
+def test_cloudwatch_rule(app):
+    app = app
     stack = AutomateWebAppHealthCheckStack(app, "automate-web-app-health-check")
     template = assertions.Template.from_stack(stack)
     template.resource_count_is("AWS::Events::Rule", 1)
     
 # check the IAM role created for lambda
-def test_lambda_role():
-    app = core.App()
+def test_lambda_role(app):
+    app = app
     stack = AutomateWebAppHealthCheckStack(app, "automate-web-app-health-check")
     template = assertions.Template.from_stack(stack)
     template.resource_count_is("AWS::IAM::Role", 1)
     
 # check the IAM policies created for lambda
-def test_lambda_policy():
-    app = core.App()
+def test_lambda_policy(app):
+    app = app
     stack = AutomateWebAppHealthCheckStack(app, "automate-web-app-health-check")
     template = assertions.Template.from_stack(stack)
     template.resource_count_is("AWS::IAM::Policy", 1)
     
 # check the cloudwatch metrics
-def test_cloudwatch_metrics():
-    app = core.App()
+def test_cloudwatch_metrics(app):
+    app = app
     stack = AutomateWebAppHealthCheckStack(app, "automate-web-app-health-check")
     template = assertions.Template.from_stack(stack)
     template.has_resource_properties("AWS::CloudWatch::Alarm", {
@@ -85,5 +91,11 @@ def test_cloudwatch_metrics():
         "MetricName": "URL_LATENCY"
     })
     
-    
-        
+# functional test to check the lambda handler function name
+# def test_lambda_handler_function():
+#     app = core.App()
+#     stack = AutomateWebAppHealthCheckStack(app, "automate-web-app-health-check")
+#     lambda_handler = stack.create_lambda
+#     assert lambda_handler is not None
+#     assert lambda_handler.function_name == "web_health_check"
+#   assert lambda_handler.runtime == lambda_.Runtime.PYTHON_3_8
